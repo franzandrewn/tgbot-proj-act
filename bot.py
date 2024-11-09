9d9c71b470cc279dd493372200da70bb1896b3c7
franzandrewn <franz.andrewn@gmail.com>
2024-11-10 02:01:00 +0300

    basic commands check

--- bot.py
+++ bot.py
@@ -1,11 +1,66 @@
 import os
-
+import logging
 import asyncio
-import telegram
+from uuid import uuid4
+
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, InlineQueryHandler
 import httpx
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logging.getLogger('httpx').setLevel(logging.WARNING)
+logging.getLogger('httpcore').setLevel(logging.WARNING)
+logging.getLogger('apscheduler').setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
 
-async def main():
+
+async def simple_api_request(news_token):
+    async with httpx.AsyncClient() as client:
+        params = {"q": "apple", "from": "2024-11-07", "to": "2024-11-07", "sortBy": "popularity"}
+        headers = {"X-Api-Key": news_token}
+        r = await client.get("https://newsapi.org/v2/everything", params=params, headers=headers)
+        if r.status_code == 200:
+            print(r.json())
+        else:
+            raise Exception("bad status code(((")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = []
+    results.append(
+        InlineQueryResultArticle(
+            id=str(uuid4()),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    await context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+
+def main():
     tg_token = os.getenv("TG_TOKEN")
     news_token = os.getenv("NEWS_TOKEN")
 
@@ -14,19 +69,22 @@ async def main():
     if news_token is None:
         raise KeyError("No news token in environmental variables")
 
-    bot = telegram.Bot(tg_token)
-    async with bot:
-        print(await bot.get_me())
+    application = ApplicationBuilder().token(tg_token).build()
 
-        async with httpx.AsyncClient() as client:
-            params = {"q": "apple", "from": "2024-11-07", "to": "2024-11-07", "sortBy": "popularity"}
-            headers = {"X-Api-Key": news_token}
-            r = await client.get("https://newsapi.org/v2/everything", params=params, headers=headers)
-            if r.status_code == 200:
-                print(r.json())
-            else:
-                print(r.status_code, r)
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    caps_handler = CommandHandler('caps', caps)
+    inline_caps_handler = InlineQueryHandler(inline_caps)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    application.add_handler(caps_handler)
+    application.add_handler(inline_caps_handler)
+    application.add_handler(unknown_handler)
+
+    application.run_polling()
 
 
 def start_bot():
-    asyncio.run(main())
+    main()
